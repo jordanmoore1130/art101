b1b0b073833dc94053f97dc0fa4cf5e2d2832f84
jordanmoore1130 <77122588+jordanmoore1130@users.noreply.github.com>
2021-02-03 15:31:22 -0800

    Lab5

---
+++ lab5/js/lab.js
@@ -0,0 +1,25 @@
+/*
+* Author: Jordan Moore
+* Created: February 3, 2021
+* License: Public Domain
+*/
+
+
+// Define Variables
+var make = "Honda";
+var model = "Pilot";
+var color = "Burgundy";
+var year = 2011;
+var ownIt= false;
+
+
+// Calculate year
+var carAge= 2021 - year;
+
+// output
+
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Owner: " + ownIt + "<br>");
+document.writeln("Age: " + carAge + "<br>");
